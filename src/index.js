60f715053cdde16284c163193dbdb38d789f0875
Tờ Tờ <taidt.hgi>
2025-02-07 16:52:06 +0700

    Query parameters

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({
@@ -20,13 +20,27 @@ app.set('views', path.join(__dirname, 'resource\\views'));
 
 console.log('PATH: ', path.join(__dirname, 'resource/views')); // PATH:  F:\F8\Node\src\resource\views
 
+// app.METHOD(PATH, HANDLER)
 // src/resource/views/home.
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/news', (req,res) => {
+app.get('/news', (req, res) => {
+    console.log(req.query.name);
+    console.log(req.query.age);
+    console.log(req.query.keywords);
     res.render('news');
+});
+
+app.get('/search', (req, res) => {
+    // console.log(req.query.q);
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    // console.log(req.query.q);
+    res.render('search');
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
